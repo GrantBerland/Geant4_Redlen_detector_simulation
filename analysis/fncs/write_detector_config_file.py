9c589cf45b386b1d53650ff410541660e3980631
GrantBerland <grant.berland.gb@gmail.com>
2019-07-10 14:41:57 -0600

    rearranging folders

--- analysis/fncs/write_detector_config_file.py
+++
@@ -1,7 +0,0 @@
-
-def writeConfigFile(pinhole_rad_mm, window_gap_mm, window_t_um, foil_t_um):
-    with open('../src/pinhole_config.txt','w') as f:
-        f.write(str(pinhole_rad_mm) + '\n')
-        f.write(str(window_gap_mm) + '\n')
-        f.write(str(window_t_um) + '\n')
-        f.write(str(foil_t_um) + '\n')
